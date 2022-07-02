41c8076df1f73e0b6bd680950a4eb5222df7c2e2
unknown <arsyatteguh@gmail.com>
2022-07-02 14:51:00 +0700

    s

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import HalamanHome from "./Component/HalamanHome";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/home" element={<HalamanHome />}></Route>
-
-        </Routes>
-      </BrowserRouter>
+    <HashRouter>
+      <HalamanHome />
+    </HashRouter>
     </>
   );
 }
