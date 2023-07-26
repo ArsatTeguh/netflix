afff77aec8208a6fef987c15784e2780a5737800
arsyatteguh <arsyatteguh@gmail.com>
2023-07-26 16:48:11 +0700

    s

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import React from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import HalamanHome from "./Component/HalamanHome";
-import {  HashRouter } from "react-router-dom";
-
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
- 
       <HalamanHome />
-   
     </>
   );
 }
